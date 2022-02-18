728db839ccddfcee54c30b8bf6efbfcb3aad4b9d
burakergun394 <burakergun394@gmail.com>
2022-02-18 17:18:19 +0300

    Rabbit-mq ile hello world testi

---
+++ RabbitMQ/sample/BasicPublisherConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿// Publisher = Producer
+
+// See https://aka.ms/new-console-template for more information
+
+
+using System.Text;
+using RabbitMQ.Client;
+
+RabbitMQ();
+
+static void RabbitMQ()
+{
+    var factory = new ConnectionFactory
+    {
+        Uri = new Uri("amqps://rnehppre:qhOPBA8pUBfGI1l9VXug3himt7ApTbu5@toad.rmq.cloudamqp.com/rnehppre")
+    };
+
+    using var connection = factory.CreateConnection();
+    var channel = connection.CreateModel();
+
+    // durable false ise memory üzerinde tutulduğu rabbit mq restart atıldıgında kuyruktaki mesajlar silinir, true ise fiziksel olarak kaydedildiği için rabbit mq restart atılsa da kuyruktaki mesajlar silinmez.
+    // exclusive true yaparsak, sadece buradaki kanal üzerinden ulaşılır, false yaparsak farklı kanallardan erişilir
+    // autoDelete false yaparsak, subscriber gittiği an kuyruk gitmesin, true yaparsak kuyruk yok olur.
+    channel.QueueDeclare(queue:"hello-queue", durable:true, exclusive:false, autoDelete: false);
+
+    var message = "hello world";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    
+    Console.WriteLine("Mesaj gönderilmiştir.");
+}
---
+++ RabbitMQ/sample/BasicSubscriberConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿// Subscriber = Consumer
+
+// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+RabbitMQ();
+
+static void RabbitMQ()
+{
+    var factory = new ConnectionFactory
+    {
+        Uri = new Uri("amqps://rnehppre:qhOPBA8pUBfGI1l9VXug3himt7ApTbu5@toad.rmq.cloudamqp.com/rnehppre")
+    };
+
+    using var connection = factory.CreateConnection();
+    var channel = connection.CreateModel();
+
+    //channel.QueueDeclare(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    /*
+     * autoAck 
+     * -> true  -> kuyruktan mesajı aldığı an silinsin,
+     * -> false -> kuyruktan mesajı aldığı an silinmesin. Mesajı eriştikten sonra manuel olarak silme işlemini yapmak gerekir
+     */
+
+    channel.BasicConsume(queue: "hello-queue", autoAck: true, consumer: consumer);
+
+    consumer.Received += (sender, e) =>
+    {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+        Console.WriteLine("Gelen Mesaj: " + message);
+    };
+
+    //consumer.Received += ConsumerOnReceived;
+}
+
+static void ConsumerOnReceived(object? sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj: " + message);
+}
