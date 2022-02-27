d78434c79d6c28f18502e7d9b8873f5fdfe4e15e
burakergun394 <burakergun394@gmail.com>
2022-02-27 15:59:43 +0300

    react hata yönetimi

--- React/btk/northwind-redux/src/redux/actions/productActions.js
+++ React/btk/northwind-redux/src/redux/actions/productActions.js
@@ -15,3 +15,49 @@ export const getProducts = (categoryId) => {
       .then((response) => dispatch(getProductsSuccess(response)));
   };
 };
+
+export const createProductSuccess = (product) => {
+  return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product };
+};
+
+export const updateProductSuccess = (product) => {
+  return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product };
+};
+
+export const saveProductApi = (product) => {
+  return fetch("http://localhost:3000/products/" + (product.id || ""), {
+    method: product.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(product),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+export const saveProduct = (product) => {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((response) => {
+        product.id
+          ? dispatch(updateProductSuccess(response))
+          : dispatch(createProductSuccess(response));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+};
+
+export const handleResponse = async (response) => {
+  if (response.ok) {
+    return response.json();
+  }
+
+  const error = await response.text();
+  throw new Error(error);
+};
+
+export const handleError = async (error) => {
+  console.log("An error occured");
+  throw new Error(error);
+};
