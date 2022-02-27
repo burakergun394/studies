af3006dec902cf2b160bb176dc840d3f77aec3a3
burakergun394 <burakergun394@gmail.com>
2022-02-27 16:50:43 +0300

    react hook kullanımı

--- React/btk/northwind-redux/src/components/root/App.js
+++ React/btk/northwind-redux/src/components/root/App.js
@@ -4,6 +4,7 @@ import Dashboard from "./Dashboard";
 import { Routes, Route } from "react-router-dom";
 import CartDetail from "../cart/CartDetail";
 import NotFound from "../common/NotFound";
+import AddOrUpdateProduct from "../products/AddOrUpdateProduct";
 
 function App() {
   return (
@@ -11,8 +12,13 @@ function App() {
       <Navi></Navi>
       <Routes>
         <Route exact path="/" element={<Dashboard />} />
-        <Route exact path="product" element={<Dashboard />} />
-        <Route exact path="cart" element={<CartDetail />} />
+        <Route exact path="/product" element={<Dashboard />} />
+        <Route exact path="/cart" element={<CartDetail />} />
+        <Route
+          exact
+          path="/saveproduct/:productId"
+          element={<AddOrUpdateProduct />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Container>
