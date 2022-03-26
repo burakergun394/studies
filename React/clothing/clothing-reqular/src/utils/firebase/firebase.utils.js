907c81300d4da9432689f32200648fe3f0c9248b
burakergun394 <burakergun394@gmail.com>
2022-03-26 22:01:43 +0300

    firebase ile kullanıcı kaydetme tamamlandı

---
+++ React/clothing/clothing-reqular/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-lrKwO69YqDx_e3t_uiX0XTN-zI1GmaM",
+  authDomain: "clothing-db-db680.firebaseapp.com",
+  projectId: "clothing-db-db680",
+  storageBucket: "clothing-db-db680.appspot.com",
+  messagingSenderId: "234556455029",
+  appId: "1:234556455029:web:3a091d4c459e6c5c6d603c",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
